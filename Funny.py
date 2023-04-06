03a769a610b8f72ced762ec9c3007a2a6947b410
S3029016 <122484341+S3029016@users.noreply.github.com>
2023-04-06 12:15:24 -0700

    Update Funny.py

--- Funny.py
+++ Funny.py
@@ -1,1 +1,9 @@
-
+# /usr/bin/python3
+import requests
+URL = "https://api.icndb.com/jokes/random"
+payload = {}
+headers = {'Accept' : 'aaplication/json'}
+response = requests.get(URL. headers=headers, data=payload)
+respJSON = response.json()
+joke = respJSON['value']['joke']
+print(joke)
